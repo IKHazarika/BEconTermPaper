9f462b9b4f0065589729056fd4fe779e865fbadd
IKHazarika <96630889+IKHazarika@users.noreply.github.com>
2022-10-28 21:03:45 +0530

    Update Market Public.R

--- Market Public.R
+++ Market Public.R
@@ -1,6 +1,11 @@
-publicSymmetricMarket$FracOfHerders <- 1 - publicSymmetricMarket$FracOfRandBid - publicSymmetricMarket$FracOfTruth
+#We use the dataset generated in the Python code file
+#The data is self generated and organised carefully to NOT require any cleaning
 
-library(texreg)
+#We define the "fraction of herders" variable described in the manuscript
+
+#We run 4 models described at length in the manuscript
+
+publicSymmetricMarket$FracOfHerders <- 1 - publicSymmetricMarket$FracOfRandBid - publicSymmetricMarket$FracOfTruth
 
 model1 <- lm(NoOfTransactions ~ NumSellers + FracOfHerders + FracOfRandBid + TypeHomophily + PositionHomophily + ValueHomophily + Density, data = publicSymmetricMarket)
 summary(model1)
@@ -14,4 +19,6 @@ summary(model3)
 model4 <- lm(NoOfTransactions ~ NumSellers + FracOfHerders + FracOfRandBid + TypeHomophily + PositionHomophily + ValueHomophily + Density + FracOfHerders*TypeHomophily + FracOfRandBid*TypeHomophily + TrueValue, data = publicSymmetricMarket)
 summary(model4)
 
+#We export the results to Latex
+library(texreg)
 texreg(list(model1, model2, model3, model4))
